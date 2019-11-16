d4d21ce01157690220634c7c8635c6d9031b7994
dpetzoldt <Dirk.Petzoldt@zalando.de>
2019-11-16 23:02:21 +0100

    Linien Layout

--- laby.py
+++ laby.py
@@ -1,36 +1,40 @@
 from random import randrange
 import pdb
 
-FELS = '*'
-WEG = ' '
-
-OBEN = (0, -1)
-UNTEN = (0, 1)
-LINKS = (-1, 0)
-RECHTS = (1, 0)
-
 REIHE = 0
 SPALTE = 1
 
-GERADE = [OBEN, UNTEN, LINKS, RECHTS]
-DIAGONAL = [(-1, -1), (-1, 1), (1, -1), (1, 1)]
+FELD = {'fels': '*',  # schwarze: ■, schraffierte: ▩
+        'weg': ' '}
 
-LINIEN = {'ensw': '┼',
-          'ens': '├',
-          'enw': '┴',
-          'esw': '┬',
-          'es': '┌',
-          'en': '└',
-          'ew': '─',
-          'e': '╶',
+GERADE = {'n': (-1, 0),  # nord
+          's': (1, 0),  # süd
+          'w': (0, -1),  # west
+          'o': (0, 1)}  # ost
+
+DIAGONAL = {'nw': (-1, -1),
+            'sw': (1, -1),
+            'os': (1, 1),
+            'on': (-1, 1)}
+
+RICHTUNG = {**GERADE, **DIAGONAL}
+
+LINIEN = {'onsw': '┼',
+          'ons': '├',
+          'onw': '┴',
+          'osw': '┬',
+          'os': '┌',
+          'on': '└',
+          'ow': '─',
+          'o': '╶',
           'nsw': '┤',
           'ns': '│',
           'nw': '┘',
           'sw': '┐',
           's': '╷',
           'n': '╵',
-          'w': '╴'}
-
+          'w': '╴',
+          '': '▩'}  # ■▪
 
 
 def wuerfeln(seiten):
@@ -39,7 +43,7 @@ def wuerfeln(seiten):
 
 def ob_fels(anteil_fels):
     if wuerfeln(100) <= anteil_fels:
-        return FELS
+        return FELD['fels']
     else:
         return " "
 
@@ -54,45 +58,47 @@ def breit(lab):
     return len(lab[0])
 
 
-def malen(lab):
+def zeige(lab):
     """Alle Felder reihenweise hinschreiben. """
     for reihe in lab:
         print(''.join(reihe))
 
 
-def neues_brett(zeilen, spalten, feld):
+def neues_brett(zeilen, spalten, inhalt=FELD['fels']):
     """Erzeuge neues Brett mit identichen Feldern"""
-    return [[feld for _ in range(0, spalten)] for _ in range(0, zeilen)]
+    return [[inhalt for _ in range(spalten)] for _ in range(zeilen)]
 
 
-def mache(feld, inhalt, brett):
+def schreibe(feld, inhalt, brett):
     """Inhalt eines Felds verändern. XXX"""
     brett[feld[0]][feld[1]] = inhalt
 
 
-def schaue(feld, brett):
+def lese(feld, brett, aussen='X'):
     """Inhalt eines Felds auslesen. """
-    return brett[feld[0]][feld[1]]
+    if feld[0] < 0 or feld[1] < 0:
+        return aussen
+    try:
+        return brett[feld[0]][feld[1]]
+    except IndexError:
+        return aussen
 
 
-def bewege(richtung, feld):
-    """Gehe vom Feld in die übergebene Richtung"""
-    return (feld[REIHE] + richtung[REIHE],
-            feld[SPALTE] + richtung[SPALTE])
+def bewege(onsw, feld):
+    """Gehe vom Feld in die übergebene Richtung (z.B. 'n')"""
+    r = RICHTUNG[onsw]
+    return feld[0] + r[0], feld[1] + r[1]
 
 
 def innen(feld, brett):
     """Prüft ob Feld weder außerhalb noch auf dem Rand liegt"""
     return 0 < feld[REIHE] < (hoch(brett) - 1) and \
-        0 < feld[SPALTE] < (breit(brett) - 1)
+           0 < feld[SPALTE] < (breit(brett) - 1)
 
 
-def nachbarn(feld, brett, diagonal=False):
+def nachbarn(feld, richtungen, brett):
     """Liste er Nachbarfelder"""
     felder = []
-    richtungen = GERADE
-    if diagonal:
-        richtungen = richtungen + DIAGONAL
     for r in richtungen:
         nachbar = bewege(r, feld)
         if innen(nachbar, brett):
@@ -104,18 +110,29 @@ def zaehle(felder, inhalt, brett):
     """Anzahl der Felder mit dem vorgegeben Inhalt"""
     anzahl = 0
     for feld in felder:
-        if schaue(feld, brett) == inhalt:
+        if lese(feld, brett) == inhalt:
             anzahl += 1
     return anzahl
 
 
 def wohin(feld, versuche, brett):
-    """Finde des nächste Welt, dass sich als Weg eignet"""
+    """Finde nächstes Feld, das sich als Weg eignet"""
+    richtungen = list(GERADE.keys())
     for versuch in range(0, versuche):
-        neu = bewege(GERADE[wuerfeln(4) - 1], feld)
+        gucke = bewege(richtungen[wuerfeln(4) - 1], feld)
         # pdb.set_trace()
-        if innen(neu, brett) and \
-                (schaue(neu, brett) == FELS) and \
-                zaehle(nachbarn(neu, brett), WEG, brett) <= 1:
-            return neu
+        if innen(gucke, brett) and \
+                (lese(gucke, brett) == FELD['fels']) and \
+                zaehle(nachbarn(gucke, GERADE, brett), FELD['weg'], brett) <= 1:
+            return gucke
     return None
+
+
+def ersetze(regel,brett):
+    """regel ist eine Funktion die ein Feld und ein Brett erwartet"""
+    ersetzt = neues_brett(hoch(brett),breit(brett))
+    for reihe in range(hoch(brett)):
+        for spalte in range(breit(brett)):
+            feld = (reihe,spalte)
+            schreibe(feld,regel(feld,brett),ersetzt)
+    return ersetzt
